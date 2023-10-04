11060d989c4a5eb8d5083174c86221377df3994c
MUHAMMED-SHAIKH-ZAHID <muhdshaikhzahid@gmail.com>
2023-10-04 13:03:29 +0530

    UI Alomost completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HeadlineCards from './components/HeadlineCards'
+import Food from './components/Food'
+import Category from './components/Category'
 
 const App = () => {
   return (
-    <div>
+    <div className='bg-black/80'>
       <Navbar />
+      <Hero/>
+      <HeadlineCards/>
+      <Food/>
+      <Category/>
     </div>
   )
 }
